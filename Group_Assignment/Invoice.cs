3756b2987cc2efd8f1650ba298b91c31231631dd
Zach Frazier <frazier.zach@gmail.com>
2018-07-27 16:41:58 -0600

    Added Logic for main window, added SQL statemets to clsMainSQL, moved hardcoded styles to app.xaml, added icon

---
+++ Group_Assignment/Invoice.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.ObjectModel;
+using System.Reflection;
+
+namespace Group_Assignment
+{
+    /// <summary>
+    /// Class that represents an invoice
+    /// </summary>
+    class Invoice
+    {
+        /// <summary>
+        /// The invoice number
+        /// </summary>
+        public string Number { get; set; }
+
+        /// <summary>
+        /// The invoice date
+        /// </summary>
+        public DateTime Date { get; set; }
+
+        /// <summary>
+        /// A collection of all the lines on the invoice
+        /// </summary>
+        public ObservableCollection<LineItem> LineItems { get; set; }
+
+        /// <summary>
+        /// The total due for all items.
+        /// </summary>
+        public string Total
+        {
+            get
+            {
+                try
+                {
+                    decimal count = 0;
+                    foreach (LineItem line in LineItems)
+                    {
+                        count += line.ItemOnLine.Price;
+                    }
+                    return string.Format("Total Due: ${0}", (count * 1.00M).ToString("F"));
+                }
+                catch (Exception ex)
+                {
+                    throw new Exception(MethodInfo.GetCurrentMethod().DeclaringType.Name + "."
+                        + MethodInfo.GetCurrentMethod().Name + " -> " + ex.Message);
+                }
+            }
+        }
+    }
+}
---
+++ Group_Assignment/Item.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Reflection;
+
+namespace Group_Assignment
+{
+    /// <summary>
+    /// Class that represents an inventory item.
+    /// </summary>
+    class Item
+    {    
+
+        /// <summary>
+        /// The code associated with the item
+        /// </summary>
+        public string Code { get; set; }
+
+        /// <summary>
+        /// The description of the item
+        /// </summary>
+        public string Description { get; set; }
+
+        /// <summary>
+        /// The price of the item.
+        /// </summary>
+        public decimal Price { get; set; }
+        
+
+        /// <summary>
+        /// The price of the item formatted as $0.00 
+        /// </summary>
+        public string PriceString
+        {
+            get
+            {
+                return this.Price.ToString("N");
+            }
+            set
+            {
+                try
+                {
+                    this.PriceString = value;
+                    value.Trim('$');
+                    value.Trim(',');
+                    this.Price = decimal.Parse(value);
+                }
+                catch (Exception ex)
+                {
+                    throw new Exception(MethodInfo.GetCurrentMethod().DeclaringType.Name + "."
+                        + MethodInfo.GetCurrentMethod().Name + " -> " + ex.Message);
+                }
+            }
+        }
+
+        /// <summary>
+        /// Returns the description of the item.
+        /// </summary>
+        /// <returns>The description of the item.</returns>
+        public override string ToString()
+        {
+            return this.Description;
+        }
+    }
+}
