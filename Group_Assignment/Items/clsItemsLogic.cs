f560541a752f9beb683d1feda10bf806bc116c82
harleytater <nathangerhardt@mail.weber.edu>
2018-07-28 20:42:00 -0600

    submitting

--- Group_Assignment/Items/clsItemsLogic.cs
+++ Group_Assignment/Items/clsItemsLogic.cs
@@ -8,5 +8,136 @@ namespace Group_Assignment.Items
 {
     class clsItemsLogic
     {
+
+
+    }
+    class Item
+
+    {
+        /// <summary>
+        /// boolean value to store if item is being used in any invoice
+        /// </summary>
+        public bool isUsed { get; set; }
+
+        /// <summary>
+
+        /// The code associated with the item
+
+        /// </summary>
+
+        public string Code { get; set; }
+
+
+
+        /// <summary>
+
+        /// The description of the item
+
+        /// </summary>
+
+        public string Description { get; set; }
+
+
+
+        /// <summary>
+
+        /// The price of the item.
+
+        /// </summary>
+
+        public decimal Price { get; set; }
+
+
+
+        /// <summary>
+
+        /// Returns the description of the item.
+
+        /// </summary>
+
+        /// <returns>The description of the item.</returns>
+
+        public override string ToString()
+
+        {
+
+            return this.Description;
+
+        }
+
+    }
+
+    class LineItem
+
+    {
+
+        /// <summary>
+
+        /// The line position of the item
+
+        /// </summary>
+
+        public int Position { get; set; }
+
+
+
+        /// <summary>
+
+        /// The quantity of the item
+
+        /// </summary>
+
+        public int Quantity { get; set; }
+
+
+
+        /// <summary>
+
+        /// The item being purchased.
+
+        /// </summary>
+
+        public Item Description { get; set; }
+
+
+
+        /// <summary>
+
+        /// The price of the item.
+
+        /// </summary>
+
+        public decimal Price { get; set; }
+
+
+
+        /// <summary>
+
+        /// The total of the line. (Quantity * Price)
+
+        /// </summary>
+
+        public decimal Total
+
+        {
+
+            get
+
+            {
+
+                return Total;
+
+            }
+
+            set
+
+            {
+
+                this.Total = this.Quantity * this.Price;
+
+            }
+
+        }
+
     }
 }
--- Group_Assignment/Items/clsItemsSQL.cs
+++ Group_Assignment/Items/clsItemsSQL.cs
@@ -8,5 +8,25 @@ namespace Group_Assignment.Items
 {
     class clsItemsSQL
     {
+        /// <summary>
+
+        /// This SQL gets all data on an invoice for a given InvoiceID.
+
+        /// </summary>
+
+        /// <param name="sInvoiceID">The InvoiceID for the invoice to retrieve all data.</param>
+
+        /// <returns>All data for the given invoice.</returns>
+
+        public string SelectInvoiceData(string sInvoiceID)
+
+        {
+
+            string sSQL = "SELECT * FROM Invoices WHERE InvoiceNum = " + sInvoiceID;
+
+            return sSQL;
+
+        }
+
     }
 }
