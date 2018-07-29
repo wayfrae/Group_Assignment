054bd6dbc8cc5bcbc874f9f1f346625aad69cc99
harleytater <nathangerhardt@mail.weber.edu>
2018-07-28 21:23:19 -0600

    added haschaned property

--- Group_Assignment/Items/clsItemsLogic.cs
+++ Group_Assignment/Items/clsItemsLogic.cs
@@ -18,6 +18,10 @@ namespace Group_Assignment.Items
         /// boolean value to store if item is being used in any invoice
         /// </summary>
         public bool isUsed { get; set; }
+        /// <summary>
+        /// boolean value to store if an item has changed and needs to be updated in the database
+        /// </summary>
+        public bool HasChanged { get; set; }
 
         /// <summary>
 
